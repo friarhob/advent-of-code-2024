6830ed5ebbe4e3821fc1bf5fcb207c938a85a9f7
Friar Hob <github@friarhob.33mail.com>
2024-12-20 18:02:13 +0000

    Day 2 part 1 solution

--- solutions/day02/day02.go
+++ solutions/day02/day02.go
@@ -1,7 +1,46 @@
 package main
 
+import (
+	"github.com/friarhob/advent-of-code-2024/utils"
+)
+
 func SolvePart1(lines []string) int {
-	return 42
+	safe_levels := 0
+	for _, line := range lines {
+		numbers := utils.StringToIntSlice(line)
+
+		is_safe := true
+		is_increasing := false
+		is_decreasing := false
+		for i := 0; i < len(numbers)-1; i++ {
+			diff := utils.AbsInt(numbers[i] - numbers[i+1])
+
+			if diff < 1 || diff > 3 {
+				is_safe = false
+				break
+			}
+
+			if numbers[i] > numbers[i+1] {
+				if is_increasing {
+					is_safe = false
+					break
+				}
+				is_decreasing = true
+			} else {
+				if is_decreasing {
+					is_safe = false
+					break
+				}
+				is_increasing = true
+			}
+		}
+
+		if is_safe {
+			safe_levels++
+		}
+	}
+
+	return safe_levels
 }
 
 func SolvePart2(lines []string) int {
